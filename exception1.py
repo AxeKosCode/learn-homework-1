3079374c518d59732993d69449adbe26635506a6
Konstantin Aksenov <axekoscode@gmail.com>
2020-03-06 22:26:26 +0300

    following files changed: exception1.py and exception2.py

--- exception1.py
+++ exception1.py
@@ -1,20 +1,48 @@
 """
-
 Домашнее задание №1
-
 Исключения: KeyboardInterrupt
-
 * Перепишите функцию ask_user() из задания while2, чтобы она 
   перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
   и завершала работу при помощи оператора break
     
 """
+my_answers = {
+    'привет' : 'Приветствую!',
+    'hello' : 'Хэллоу!',
+    'hi' : 'Хай!',
+    'как дела' : 'Хорошо!',
+    'что делаешь' : 'Программирую',
+    'где ты живешь' : 'В твоём компьютере',
+    'сколько тебе лет' : 'Я не веду такую статистику'
+}
+
+user_questions = []
 
 def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
+    while True:
+        try:
+            question = input('  Спрашивай, что хочешь: ').lower().strip('?! ')
+        except KeyboardInterrupt:
+            print('   Пока!')
+            break
+
+        if question:
+            if question == 'exit' or question == 'выход':
+                print('До встречи!')
+                break
+            elif question in my_answers:
+                print(my_answers[question])
+            elif question in user_questions:
+                print('Ты вроде уже спрашивал это')
+            else:
+                user_questions.append(question)
+                print('У меня нет ответа на этот вопрос')
+        else:
+            print('Ты ничего не спросил')
+    # print('user_questions =', user_questions)
     
 if __name__ == "__main__":
     ask_user()
--- exception2.py
+++ exception2.py
@@ -1,9 +1,6 @@
 """
-
 Домашнее задание №1
-
 Исключения: приведение типов
-
 * Напишите функцию get_summ(num_one, num_two), которая принимает 
   на вход два целых числа (int), складывает их и возвращает результат 
   сложения
@@ -16,7 +13,17 @@ def get_summ(num_one, num_two):
     """
     Замените pass на ваш код
     """
-    pass
+    try:
+        one = int(num_one)
+    except ValueError:
+        return 'Первый аргумент должен быть числом!'
+    try:
+        two = int(num_two)
+    except ValueError:
+        return 'Второй аргумент должен быть числом!'
+    
+    return one + two
+
     
 if __name__ == "__main__":
     print(get_summ(2, 2))
@@ -24,3 +31,4 @@ if __name__ == "__main__":
     print(get_summ("4", "4"))
     print(get_summ("five", 5))
     print(get_summ("six", "шесть"))
+    print(get_summ(10, "ten"))
